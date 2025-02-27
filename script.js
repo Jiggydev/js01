ca33a2609fc06a119ec5f6c10f882461ed6b2f63
JiggyDev <jeradybrew18@gmail.com>
2025-02-27 10:58:21 +0300

    commit

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-let num1 = 5;
 
-let num2 = 150 ;
+let fullName = "AJ Mensah";
+let age = 27;
+let student = false;
 
-let sum = num1 + num2;
-
-console.log(sum) ;
-
-
-let age = 25;
-
-console.log(`you are ${age} years old`);
-
-
-
-let price = 19.99;
-
-
-let quantity = 5;
-
-let numericTotal = price * quantity;
-
-
-
-
-console.log(`The Total is $${numericTotal.toFixed(2)} would you like to pay card or cash?`);
-
-let firstName = "yasmine";
-
-console.log(firstName)
-
-
-console.log(typeof firstName);
-
-
+document .getElementById("p1") .textContent = fullName;
