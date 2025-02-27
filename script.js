4d2c485ac3f79ef54dbfff5e0d6050ba93338cef
JiggyDev <jeradybrew18@gmail.com>
2025-02-27 10:00:25 +0300

    jso1

---
+++ script.js
@@ -0,0 +1,35 @@
+let num1 = 5;
+
+let num2 = 150 ;
+
+let sum = num1 + num2;
+
+console.log(sum) ;
+
+
+let age = 25;
+
+console.log(`you are ${age} years old`);
+
+
+
+let price = 19.99;
+
+
+let quantity = 5;
+
+let numericTotal = price * quantity;
+
+
+
+
+console.log(`The Total is $${numericTotal.toFixed(2)} would you like to pay card or cash?`);
+
+let firstName = "yasmine";
+
+console.log(firstName)
+
+
+console.log(typeof firstName);
+
+
