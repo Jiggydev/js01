03ac85ec6897713b3e7fec029f53d2747e91df29
JiggyDev <jeradybrew18@gmail.com>
2025-03-02 16:57:43 +0300

    learning

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "yasimne";
+
+console.log(firstName)
+
+
+let price = 10.99;
+let quantity = 50;
+let total = price * quantity;
+
+console.log(`your total price is $${total}`);
+
+
+console.log(typeof firstName);
--- script.js
+++
@@ -1,6 +0,0 @@
-
-let fullName = "AJ Mensah";
-let age = 27;
-let student = false;
-
-document .getElementById("p1") .textContent = fullName;
